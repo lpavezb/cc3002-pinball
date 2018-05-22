ec8a84277c1d6886658fc1d0151ff70afb7e2156
lpavezb <lukas.pavez@ug.uchile.cl>
2018-05-22 01:37:32 -0400

    :cat::snake: add null table test :snake::cat:

---
+++ Pinball/src/test/java/NullTableTest.java
@@ -0,0 +1,42 @@
+import logic.table.NullTable;
+import logic.table.Table;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class NullTableTest {
+    private Table table;
+
+    @Before
+    public void setUp(){
+        table = new NullTable();
+    }
+
+    @Test
+    public void getName(){
+        assertEquals("There is no table", table.getTableName());
+    }
+
+    @Test
+    public void getElements(){
+        assertEquals(0, table.getElements().size());
+    }
+
+    @Test
+    public void targets(){
+        assertEquals(0, table.getTargets().size());
+        assertEquals(0, table.getSpotTargets().size());
+        assertEquals(0, table.getDropTargets().size());
+
+        assertEquals(0, table.getCurrentlyDroppedDropTargets());
+        assertEquals(0, table.getNumberOfDropTargets());
+    }
+
+    @Test
+    public void bumpers(){
+        assertEquals(0, table.getBumpers().size());
+        assertEquals(0, table.getKickerBumpers().size());
+        assertEquals(0, table.getPopBumpers().size());
+    }
+}
